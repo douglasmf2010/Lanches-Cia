f5c4e788b350a76dc9e17121bd4811ae7340b7a6
DOUGLAS MATTOZO FERNANDES <douglas_fernandes151@estudante.sesisenai.org.br>
2026-04-20 17:06:41 -0300

    cardapio feito

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require("sequelize");
+
+const {
+    PSQL_USER, PSQL_PASS, PSQL_HOST, PSQL_PORT, PSQL_DATABASE
+} = process.env
+
+const sequelizeConn = new Sequelize(`
+    postgres://${PSQL_USER}:${PSQL_PASS}@${PSQL_HOST}:${PSQL_PORT}/${PSQL_DATABASE}`
+);
+
+module.exports = sequelizeConn;
---
+++ src/config/routes.js
@@ -0,0 +1,20 @@
+const ItemController = require('../controllers/item_controllers')
+
+function route(app){
+     app.get("/item", (req, res)=>{
+        ItemController.index(req, res)
+     });
+     app.get("/item/:id", (req, res)=>{
+        ItemController.show(req, res)
+     });
+     app.post('/item', (req, res)=>{
+        ItemController.create(req, res);
+     });
+     app.put('/item/:id', (req, res)=>{
+        ItemController.update(req, res);
+     });
+     app.delete('/item/:id', (req, res)=>{
+        ItemController.delete(req, res);
+     })
+}
+module.exports = route
